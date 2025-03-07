79b5b0c9e56d04aedf99230a7eec49c409fc631d
Ayaanuddin29 <khajaayaan2912@gmail.com>
2025-03-07 15:18:17 +0530

    Ok

--- app/dashboard/interview/[interviewId]/start/_components/QuestionSection.jsx
+++ app/dashboard/interview/[interviewId]/start/_components/QuestionSection.jsx
@@ -13,7 +13,7 @@ function QuestionSection({mockInterviewQuestion,activeQuestion,setActiveQuestion
   }
   return mockInterviewQuestion && (
     <div className='p-5 border rounded-lg my-10'>
-     <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 ' >
+     <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5' >
         {mockInterviewQuestion && mockInterviewQuestion.map((question,index)=>(
             <h2 className={`p-2 rounded-full text-xs md:text-sm text-center cursor-pointer ${activeQuestion===index && 'bg-blue-800 text-white'}`} onClick={()=>setActiveQuestion(index)}>Question #{index+1}</h2>
         ))}
