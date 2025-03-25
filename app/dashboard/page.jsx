4f487900ea7cadef44ab39bf4869d2bb21a5ee0b
Ayaanuddin29 <148215195+Ayaanuddin29@users.noreply.github.com>
2025-03-25 13:02:05 +0530

    Update page.jsx
    Ok

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -13,19 +13,6 @@ function Dashboard() {
     </div>
     {/* Previous Interview List */}
     <InterviewList/>
-<!--Start of Tawk.to Script-->
-<script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/67e2598353a241190a02d7af/1in62rb14';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-</script>
-<!--End of Tawk.to Script-->
     </div>
   )
 }
